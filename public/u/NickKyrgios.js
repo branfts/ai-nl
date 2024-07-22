c462930f8b1591bb4a80551347c90c84ae578ffe
branfts <support@june07.com>
2024-07-22 16:27:15 -0700

    Updated by A10x Service

---
+++ public/u/NickKyrgios.js
@@ -0,0 +1,10 @@
+
+const links = [
+    {
+        "url": "https://x.com/NickKyrgios",
+        "title": "Twitter",
+        "subtitle": "Fuck X"
+    }
+];
+const rules = [];
+export { links, rules };
