0c27c26a1590109dab19a3e61c05b756ce2c62c3
BraNFT™ <99556664+branfts@users.noreply.github.com>
2024-07-22 20:29:20 -0700

    Update NickKyrgios.js

--- public/u/NickKyrgios.js
+++ public/u/NickKyrgios.js
@@ -1,9 +1,29 @@
 
 const links = [
     {
+        "url": "https://linktr.ee/NickKyrgios",
+        "title": "Nicholas Kyrgios's Linktr.ee",
+    },
+    {
         "url": "https://x.com/NickKyrgios",
-        "title": "Twitter",
-        "subtitle": "Fuck X"
+    },
+    {
+        "url": "https://onlyfans.com/k1ngkyrg1os",
+    },
+    {
+        "url": "https://tiktok.com/@nickkyrgios",
+    },
+    {
+        "url": "https://x.com/NickKyrgios",
+    },
+    {
+        "url": "https://instagram.com/k1ngkyrg1os",
+    },
+    {
+        "url": "https://www.facebook.com/nickkyrgiosfoundation/",
+    },
+    {
+        "url": "mailto:daniel@thisisevolve.com",
     }
 ];
 const rules = [];
