87696ab513d7d8b8dc08b4f963bf321f313cf144
branfts <support@june07.com>
2024-07-24 18:25:41 -0700

    Updated by A10x Service

---
+++ public/u/nanmahtwee.js
@@ -0,0 +1,20 @@
+
+const links = [
+    {
+        "url": "https://www.tiktok.com/@nanmahtwe_"
+    },
+    {
+        "url": "https://www.youtube.com/channel/UCxlnctfFe4okO9MdBEV2NQg"
+    },
+    {
+        "url": "https://www.facebook.com/AnnNangNan98"
+    },
+    {
+        "url": "https://x.com/nanmahtwee"
+    },
+    {
+        "url": "https://www.instagram.com/nanmahtwee"
+    }
+];
+const rules = [];
+export { links, rules };
